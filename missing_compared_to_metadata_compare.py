d8bd256c4676820e3dd1a28b8508a04f53643a51
Ngozi Harrison <ngozih@g.ucla.edu>
2024-06-17 13:37:08 -0700

    joined csv checking missing against metadata and merritt items

--- missing_compared_to_metadata_compare.py
+++ missing_compared_to_metadata_compare.py
@@ -8,15 +8,19 @@ full_metadata = pd.read_csv("file-lists/pm_export_2022-12-08_combined_dlp (1).cs
 
 
 missing_box_items = pd.read_csv("file-lists/missing.csv")
+missing_box_items ["file"] = missing_box_items ["file"].apply(lambda x: x[:-4])
 
 
-Box_Contents_Results_Compared_To_Metadata_Record = pd.DataFrame(columns=['Local ID', 'Metadata Record'])
+Box_Contents_Results_Compared_To_Metadata = pd.DataFrame(columns=['file', 'Has Metadata record'])
+# Are there any images in the missing folder that are either/both: also in Merritt DO NOT have a metadata record
+#the merrit comparison is an image to image comparison and the metadata comparison is a record level comparison
+for index, row in missing_box_items.iterrows():
+    result = [row["file"], ""]
+    print(row["file"])
+    if len(full_metadata[full_metadata["local ID 2"] == row["object"]]) >= 1:
+        result[1] = "Yes"
+        Box_Contents_Results_Compared_To_Metadata.loc[index] = result
+            
 
-for index, row in missing_box_items.head(1000).iterrows():
-    result = [row["object"],""]
-    if len(full_metadata[full_metadata["local ID"] == row["object"]]) == 0:
-        result[2] = "No"
-    Box_Contents_Results_Compared_To_Metadata_Record.loc[index] = result
+Box_Contents_Results_Compared_To_Metadata.to_csv("Box_Contents_Results_Compared_To_Metadata.csv", index= False)
 
-Box_Contents_Results_Compared_To_Metadata_Record.to_csv("Box_Contents_Results_Compared_To_Metadata_Record.csv", index= False)
-    
