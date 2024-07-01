b75cf0bf79fc6beb015ff8703149bb3234d27ceb
Ngozi Harrison <ngozih@g.ucla.edu>
2024-07-01 13:14:23 -0700

    Updated mrt-erc.txt files with who field

--- metadata_update_packet.py
+++ metadata_update_packet.py
@@ -7,13 +7,14 @@ path = "/Users/ngoziharrison/Documents/palmu-data/merritt-updates"
 
 txt_template = """erc: 
 what:{title} | {alt_title} ; {collection}
+who: no data
 where: {ark}
 where: {link}"""
 
 for index, row in df.iterrows():
     folder = row["where"]
     print(folder)
-    os.mkdir(os.path.join(path, folder))
+    os.makedirs(os.path.join(path, folder), exist_ok = True)
 
     with open(path + "/" + folder + "/mrt-erc.txt", "w") as file:
         file.write(txt_template.format(title = row[5], alt_title = row[6], ark = row[1], collection = row[2], link = row[4]))
