1f922181ef1ed4ceab181143156a32fd092281c5
Ngozi Harrison <ngozih@g.ucla.edu>
2024-07-03 12:30:33 -0700

    moved files

--- metadata_update_packet.py
+++ metadata_update_packet.py
@@ -3,7 +3,7 @@ import os
 
 df = pd.read_csv("merritt-updates/palmu_metadata_delivery1-merrit-update.csv")
 
-path = "/Users/ngoziharrison/Documents/palmu-data/merritt-updates"
+path = "/Users/ngoziharrison/Documents/palmu-data/merritt-updates/ingest-process"
 
 txt_template = """erc: 
 what:{title} | {alt_title} ; {collection}
@@ -18,3 +18,4 @@ for index, row in df.iterrows():
 
     with open(path + "/" + folder + "/mrt-erc.txt", "w") as file:
         file.write(txt_template.format(title = row[5], alt_title = row[6], ark = row[1], collection = row[2], link = row[4]))
+
